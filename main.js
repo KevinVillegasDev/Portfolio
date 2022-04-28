1a38d3855954c050dcd8d3ec8c7c507c1ba153a5
Kevin Villegas <kevin.villegas94@gmail.com>
2022-04-28 14:44:23 -0700

    added hamburger menu, ajax call to hide/show

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+$(".nav-list").toggleClass("hide");
+$(".nav-menu img").on("click", function () {
+    $(".nav-list").toggleClass("hide");
+});
+
 let googleForms =
     "https://docs.google.com/forms/u/0/d/e/1FAIpQLSfnisF_bBgsm8dQtBDXWN3lmptb33D3UFR96LGMFkSNJwEx_Q/formResponse";
 
@@ -23,10 +28,10 @@ $("form").on("submit", function (form) {
         url: url,
     })
         .then(function (e) {
-            alert("Form submitted! Thank you!");
+            alert("Form submitted! Thank you! I will get back to you shortly.");
         })
         .catch(function (e) {
-            console.error("Form failed!");
+            alert("Form submission failed. Please try again");
         });
 
     return false;
