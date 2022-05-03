3e54e85455e1e5457d7650e6bdadc1ead2c0bbd3
Kevin Villegas <kevin.villegas94@gmail.com>
2022-05-03 09:03:13 -0700

    added credit to jacob

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ $(".nav-list").toggleClass("hide");
 $(".nav-menu img").on("click", function () {
     $(".nav-list").toggleClass("hide");
 });
-
+//thanks to Jacob for getting me set up with the form api below!
 let googleForms =
     "https://docs.google.com/forms/u/0/d/e/1FAIpQLSfnisF_bBgsm8dQtBDXWN3lmptb33D3UFR96LGMFkSNJwEx_Q/formResponse";
 
